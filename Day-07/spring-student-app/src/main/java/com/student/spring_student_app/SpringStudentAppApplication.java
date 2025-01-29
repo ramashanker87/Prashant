fd51d7c9b95aa2cd04eeea635d38580449e688bd
admin <prashant@gmail.com>
2025-01-28 20:03:48 -0800

    Day 7

---
+++ Day-07/spring-student-app/src/main/java/com/student/spring_student_app/SpringStudentAppApplication.java
@@ -0,0 +1,15 @@
+package com.student.spring_student_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringStudentAppApplication.class, args);
+
+	}
+
+}
