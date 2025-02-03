561450f1160bcbdcaa8ad5ddccd344a8a083f5c7
admin <prashant@gmail.com>
2025-02-02 19:45:38 -0800

    patient application

---
+++ Day-09/basic-spring-security/src/main/java/com/prashant/app/BasicSpringSecurityApplication.java
@@ -0,0 +1,25 @@
+package com.prashant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+
+@SpringBootApplication
+@EnableScheduling
+public class BasicSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringSecurityApplication.class, args);
+
+	}
+
+	@Scheduled(fixedRate = 5000)
+	public void scheduleTask(){
+		System.out.println("Task executed at : "+ new Date());
+	}
+
+
+}
