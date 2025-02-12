d30926f7a905b75d74760892cbf47621dacf9479
admin <rama@gail.com>
2025-02-12 03:06:14 -0800

    Day 17 assignment

--- Day-16/student-management-app/rabbitmq-student-consumer/src/main/java/com/prashant/app/repository/StudentRepo.java
+++ Day-16/student-management-app/rabbitmq-student-consumer/src/main/java/com/prashant/app/repository/StudentRepo.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepositiory extends JpaRepository<Student, Integer> {
-
-
+public interface StudentRepo extends JpaRepository<Student, Integer> {
     Student findByName(String name);
 }
